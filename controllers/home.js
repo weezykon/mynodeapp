3ca075994c722e796af0b015d916eef2149d7c99
Akinbode Weezykon <weezykon@gmail.com>
2019-05-26 02:36:30 +0100

    submit form

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,9 @@
 exports.home = function(req, res, next) {
     res.render('index', { title: 'Express' });
 };
+
+exports.submit_lead = function(req, res, next) {
+    console.log("lead email:", req.body.lead_email);
+    res.redirect('/');
+    console.log('got here');
+};
