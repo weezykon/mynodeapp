b01419274c2e8f8b54b4ccdc4f91c9bfd20413d0
Akinbode Weezykon <weezykon@gmail.com>
2019-05-26 02:53:17 +0100

    sequelize

---
+++ config/config.js
@@ -0,0 +1,24 @@
+{
+  "development": {
+    "username": "express-mynodeapp-dbuser",
+    "password": '12345',
+    "database": "express-mynodeapp-db",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "post": 5432
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
