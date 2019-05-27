9fedf4f4ac8fdf30f99cb65b91c8fab514ee5f60
Akinbode Weezykon <weezykon@gmail.com>
2019-05-28 00:47:21 +0100

    fixed bugs

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,13 @@
+const models = require('../models');
 exports.home = function(req, res, next) {
     res.render('index', { title: 'Express' });
 };
 
 exports.submit_lead = function(req, res, next) {
-    console.log("lead email:", req.body.lead_email);
-    res.redirect('/');
-    console.log('got here');
+    // console.log("lead email:", req.body.lead_email);
+    return models.Lead.create({
+        email: req.body.lead_email
+    }).then( lead => {
+        res.redirect('/');
+    })
 };
