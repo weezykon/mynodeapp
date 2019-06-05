568850a7c13b637736e4313e2ed9855ad6be65f4
Akinbode Weezykon <weezykon@gmail.com>
2019-06-05 17:12:05 +0100

    new migration

---
+++ migrations/20190604120304-user.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.createTable('Users', {
+      id: {
+          allowNull: false,
+          primaryKey: true,
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.UUIDV4,
+      },
+      createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+      },
+      updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+      },
+      email: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          unique: true
+      },
+      password: {
+          allowNull: false,
+          type: Sequelize.STRING
+      },
+      username: {
+          allowNull: false,
+          type: Sequelize.STRING
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.dropTable('Users');
+  }
+};
