72847d8256a11ea6bba2b2415a55547e1e4c1297
Akinbode Weezykon <weezykon@gmail.com>
2019-06-06 01:18:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+let passport = require('passport');
+let session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var landingRouter = require('./routes/landing');
+
+require('./passport_setup')(passport);
 
 var app = express();
 
@@ -19,6 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({secret: 'our secret'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
