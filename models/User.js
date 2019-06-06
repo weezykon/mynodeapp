6797d470cc0038a135e354131182383c751a21b0
Akinbode Weezykon <weezykon@gmail.com>
2019-06-06 01:15:45 +0100

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,24 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+    var User = sequelize.define('User', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+    return User;
+}
