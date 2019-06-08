5d65457a47eb041146e8cd2b23e8e6231228a0f7
Akinbode Weezykon <weezykon@gmail.com>
2019-06-08 22:20:17 +0100

    render errors

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,9 @@ let bcrypt = require('bcrypt');
 const passport = require('passport');
 const myPassport = require('../passport_setup');
 let flash = require('connect-flash');
+const {isEmpty} = require('lodash');
+const {validateUser} = require('../validators/signup');
+const {loginUser} = require('../validators/login');
 
 exports.show_login = function(req, res, next) {
     res.render('login', { formData: {}, errors: {} });
@@ -12,34 +15,57 @@ exports.show_signup = function(req, res, next) {
     res.render('signup', { formData: {}, errors: {} });
 };
 
+const rerender_signup = function(errors, req, res, next) {
+    res.render('signup', { formData: req.body, errors: errors });
+};
+
+const rerender_login = function(errors, req, res, next) {
+    res.render('login', { formData: req.body, errors: errors });
+};
+
 const generateHash = function(password){
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null); 
 }
 
 exports.signup = function(req, res, next) {
-    const newUser = models.User.build({
-        email: req.body.email,
-        username: req.body.username,
-        password: generateHash(req.body.password)
-    });
-    return newUser.save().then(result => {
-        passport.authenticate('local', {
-            successRedirect: "/",
-            failureRedirect: "/signup",
-            failureFlash: true
-        })(req, res, next);
-    }).catch(function(err) {
-        // print the error details
-        console.log(err, req.body.email);
+    let errors = {};
+    return validateUser(errors, req).then(errors =>{
+        if (!isEmpty(errors)) {
+            rerender_signup(errors, req, res, next);
+        } else {
+            const newUser = models.User.build({
+                email: req.body.email,
+                username: req.body.username,
+                password: generateHash(req.body.password)
+            });
+            return newUser.save().then(result => {
+                passport.authenticate('local', {
+                    successRedirect: "/",
+                    failureRedirect: "/signup",
+                    failureFlash: true
+                })(req, res, next);
+            }).catch(function(err) {
+                // print the error details
+                console.log(err, req.body.email);
+            });
+        }
     });
 }
 
 exports.login = function(req, res, next) {
-    passport.authenticate('local', {
-        successRedirect: "/",
-        failureRedirect: "/login",
-        failureFlash: true
-    })(req, res, next);
+    let errors = {};
+    return loginUser(errors, req).then(errors =>{
+        if (!isEmpty(errors)) {
+            rerender_login(errors, req, res, next);
+            console.log(errors);
+        } else {
+            // passport.authenticate('local', {
+            //     successRedirect: "/",
+            //     failureRedirect: "/login",
+            //     failureFlash: true
+            // })(req, res, next);
+        }
+    });
 }
 
 exports.logout = function(req, res, next) {
